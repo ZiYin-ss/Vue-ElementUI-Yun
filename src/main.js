6487ef6301a3fdf61415a12d45ad410256d00b5c
Zhao Qiang <980689346@qq.com>
2021-10-25 14:31:43 +0800

    基恩结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
+import Vue from 'vue'  // 这个import是es6的语法
 import App from './App.vue'
+import router from  './router'
 
+// 关闭控制台提示 浏览器里面
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
