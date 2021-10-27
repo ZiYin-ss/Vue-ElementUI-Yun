54ab8e53c8c7df62f240f680de93bb30394b29be
Zhao Qiang <980689346@qq.com>
2021-10-27 23:23:23 +0800

    aaa

--- src/components/sys/basic/DepMana.vue
+++ src/components/sys/basic/DepMana.vue
@@ -1,15 +1,162 @@
 <template>
-  <div>
-    部门管理
+  <div style="width: 500px;">
+    <el-input placeholder="请输入部门名称进行搜索" prefix-icon="el-search" v-model="filterText">
+    </el-input>
+
+    <el-tree class="filter-tree" :data="deps" :props="defaultProps" :filter-node-method="filterNode" ref="tree" :expand-on-click-node="false">
+      <span class="custom-tree-node" slot-scope="{ node, data }" style="display: flex;justify-content: space-between;width: 100%;">
+        <span>{{ data.name }}</span>
+        <span>
+          <el-button class="depBtn" type="primary" size="mini" @click="() => showAddDep(data)">
+            添加部门
+          </el-button>
+          <el-button type="danger" class="depBtn" size="mini" @click="() => deleteDep(data)">
+            删除部门
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+    <el-dialog
+        title="添加部门" :visible.sync="dialogVisible" width="30%">
+      <div>
+        <table>
+          <tr>
+            <td>
+              <el-tag>上级部门</el-tag>
+            </td>
+            <td>{{pname}}</td>
+          </tr>
+          <tr>
+            <td>
+              <el-tag>部门名称</el-tag>
+            </td>
+            <td>
+              <el-input v-model="dep.name" placeholder="请输入部门名称"> </el-input>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="doAddDep">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-export default {
-  name: "DepMana"
-}
+  export default {
+      name: "DepMana",
+      data(){
+      return {
+        filterText:'',
+        deps:[],
+        defaultProps: {
+          children: 'children',
+          label: 'name'
+        },
+        dialogVisible:false,
+        dep:{
+          name:'',
+          parentId:-1
+        },
+        pname:''
+      }
+      },
+      mounted() {
+        this.initDeps()
+      },
+      methods: {
+        removeDepFromDeps(p,deps,id){  //手动删除
+          for (let i=0;i<deps.length;i++){
+            let d=deps[i];
+            if(d.id = id){
+              deps.splice(i,1)
+              if(deps.length==0){
+                p.isParent=false
+              }
+              return
+            }else {
+              this.removeDepFromDeps(d,d.children,id)
+            }
+          }
+        },
+        deleteDep(data){
+          if(data.isParent){
+            this.$message.error('父部门删除失败')
+          }else {
+            this.$confirm('此操作将永久删除['+data.name+']该部门, 是否继续?', '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              this.deleteRequest('/system/basic/department/'+data.id).then(resp=>{
+                if(resp){
+                  this.removeDepFromDeps(this.deps,data.id);
+                }
+              })
+            }).catch(() => {
+              this.$message({
+                type: 'info',
+                message: '已取消删除'
+              });
+            });
+          }
+        },
+        showAddDep(data){
+          this.dep.parentId = data.id
+          this.pname = data.name
+          this.dialogVisible = true
+        },
+        initDep(){
+          this.dep={
+            name:'',
+            parentId: -1
+          }
+          this.pname = '';
+        },
+        addDep2Dep2(deps,dep){  //插入 先找deps的id和dep的id是否是不是一样 然后再去子元素里面找 递归
+          for (let i=0;i<deps.length;i++){
+            let d=deps[i];
+            if(d.id===dep.parentId){
+              d.children = d.children.concat(dep)
+              return
+            }else {
+              this.addDep2Dep2(null,d.children,dep)
+            }
+          }
+        },
+        doAddDep(){
+          this.postRequest('/system/basic/department/',this.dep).then(resp=>{
+            if(resp){
+              this.addDep2Dep2(this.deps,resp.obj); //添加到deps里面 因为不是调用initDeps
+              this.dialogVisible = false;
+              this.initDep()  // 下次添加的时候 要把这次的清空
+            }
+          })
+        },
+        initDeps(){
+            this.getRequest('/system/basic/department/').then(resp=>{
+              if(resp){
+                this.deps = resp
+              }
+            })
+          },
+        filterNode(value, data) {
+            if (!value) return true;
+            return data.name.indexOf(value) !== -1;
+          }
+        },
+      watch: {
+        filterText(val) {
+          this.$refs.tree.filter(val);
+        }
+      },
+  }
 </script>
 
 <style scoped>
-
+  .depBtn{
+    margin-left: 2px;
+  }
 </style>
--- src/views/sys/SysAdmin.vue
+++ src/views/sys/SysAdmin.vue
@@ -1,15 +1,126 @@
 <template>
   <div>
-    操作员管理
+    <div style="display: flex;justify-content: center;margin-top: 10px;">
+      <el-input v-model="keywords" placeholder="通过用户名搜索用户..." prefix-icon="el-icon-search" style="width: 400px;margin-right: 10px;"></el-input>
+      <el-button icon="el-icon-search" @click="doSearch" type="primary">搜索</el-button>
+    </div>
+    <div class="admin-container">
+      <el-card class="admin-card" v-for="(admin,index) in admins">
+        <div slot="header" class="clearfix">
+          <span>{{admin.name}}</span>
+          <el-button style="float: right; padding: 3px 0;color: red" icon="el-icon-delete" @click="deleteAdmin(admin)" type="text"></el-button>
+        </div>
+        <div>
+          <div class="img-container">
+            <img :src="admin.userFace" class="userFace-img" :alt="admin.name" :title="admin.name">
+          </div>
+        </div>
+        <div class="userinfo-content">
+          <div>用户名:{{admin.name}}</div>
+          <div>手机号码:{{admin.phone}}</div>
+          <div>电话号码:{{admin.telephone}}</div>
+          <div>地址:{{admin.address}}</div>
+          <div>用户状态:
+            <el-switch
+              style="display: block"
+              v-model="admin.enabled"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+              @change="enabledChange(admin)"
+              active-text="启用"
+              inactive-text="禁用">
+            </el-switch>
+          </div>
+          <div>
+            用户角色:
+            <el-tag style="margin-right: 10px;" type="success" v-for="(role,index) in admin.roles" :key="index">{{role.nameZh}}</el-tag>
+            <el-button type="text" icon="el-icon-more">buttonCont</el-button>
+          </div>
+          <div>
+            备注:{{admin.remark}}
+          </div>
+        </div>
+      </el-card>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SysAdmin"
+  name: "SysAdmin",
+  data(){
+    return {
+      admins:[],
+      keywords:''
+    }
+  },
+  mounted() {
+    this.initAdmins()
+  },
+  methods:{
+    enabledChange(admin){
+      // 这个地方是把原来的东西都加上了 然后作为更新 switch的值也更新了
+      this.putRequest('/system/admin/',admin).then(resp=>{
+        if(resp){
+          this.initAdmins()
+        }
+      })
+    },
+    deleteAdmin(admin){
+      this.$confirm('此操作将永久删除['+admin.name+']操作员, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.deleteRequest('/system/admin/'+admin.id).then(resp=>{
+          if(resp){
+            this.initAdmins()
+          }
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+    doSearch(){
+      this.initAdmins()
+    },
+    initAdmins(){
+      this.getRequest('/system/admin/?keywords='+this.keywords).then(resp=>{
+        if(resp){
+          this.admins = resp
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .admin-card{
+    width: 350px;
+    margin-bottom: 20px;
+  }
+  .admin-container{
+    display: flex;
+    margin-top: 10px;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+  .userFace-img{
+    width: 72px;
+    height: 72px;
+    border-radius: 20px;
+  }
+  .img-container{
+    width: 100%;
+    display: flex;
+    justify-self: center;
+  }
+  .userinfo-content{
+    font-size: 12px;
+    color: #409eff;
+  }
 </style>
