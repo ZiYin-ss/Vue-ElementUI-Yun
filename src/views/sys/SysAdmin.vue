f5a7b8e3aae90027b277fe962eb44f439a36ee72
Zhao Qiang <980689346@qq.com>
2021-10-28 00:53:26 +0800

    10/27

--- src/views/sys/SysAdmin.vue
+++ src/views/sys/SysAdmin.vue
@@ -21,20 +21,18 @@
           <div>电话号码:{{admin.telephone}}</div>
           <div>地址:{{admin.address}}</div>
           <div>用户状态:
-            <el-switch
-              style="display: block"
-              v-model="admin.enabled"
-              active-color="#13ce66"
-              inactive-color="#ff4949"
-              @change="enabledChange(admin)"
-              active-text="启用"
-              inactive-text="禁用">
+            <el-switch style="display: block" v-model="admin.enabled" active-color="#13ce66" inactive-color="#ff4949" @change="enabledChange(admin)" active-text="启用" inactive-text="禁用">
             </el-switch>
           </div>
           <div>
             用户角色:
             <el-tag style="margin-right: 10px;" type="success" v-for="(role,index) in admin.roles" :key="index">{{role.nameZh}}</el-tag>
-            <el-button type="text" icon="el-icon-more">buttonCont</el-button>
+            <el-popover placement="right" title="角色列表" width="200" @show="showPop(admin)"  @hide="hidePop(admin)" trigger="click">
+              <el-select v-model="selectRoles" multiple placeholder="请选择">
+                <el-option v-for="(r,index) in allRoles" :key="index" :label="r.nameZh" :value="r.id"></el-option>
+              </el-select>
+              <el-button slot="reference" type="text" icon="el-icon-more"></el-button>
+            </el-popover>
           </div>
           <div>
             备注:{{admin.remark}}
@@ -51,13 +49,64 @@ export default {
   data(){
     return {
       admins:[],
-      keywords:''
+      keywords:'',
+      allRoles:[],
+      selectRoles:[]
     }
   },
   mounted() {
     this.initAdmins()
   },
   methods:{
+
+    hidePop(admin){
+      let roles = []
+      Object.assign(roles,admin.roles);
+      let flag = false
+      if(roles.length === this.selectRoles.length){
+        flag=true
+      }else {
+        for(let i=0;i<roles.length;i++){
+          let role =roles[i];
+          for(let j=0;j<this.selectRoles.length;j++){
+            let sr = this.selectRoles[j];
+            if(role.id===sr){
+              roles.splice(i,1)
+              i--;
+              break;
+            }
+          }
+        }
+        if(roles.length!==0){
+          flag = true
+        }
+      }
+
+
+      if(flag){
+        let url = '/system/admin/role?adminId='+admin.id
+        this.selectRoles.forEach(sr=>{
+          url+='&rids='+sr;
+        })
+        this.putRequest(url).then(resp=>{
+          if (resp){
+            this.initAdmins()
+          }
+        })
+      }
+    },
+    showPop(admin){
+      this.getRequest('/system/admin/roles').then(resp=>{
+        if(resp){
+          this.allRoles = resq
+        }
+      })
+      let roles = admin.roles
+      this.selectRoles = []
+      roles.forEach(r=>{
+        this.selectRoles.push(r.id)
+      })
+    },
     enabledChange(admin){
       // 这个地方是把原来的东西都加上了 然后作为更新 switch的值也更新了
       this.putRequest('/system/admin/',admin).then(resp=>{
